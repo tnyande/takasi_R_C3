01b5d5dfbc4fc149856673fd560c56b2aa9b98d7
Takasi Nyande <tnyande@gmail.com>
2023-08-23 01:08:04 +0000

    React azubi task 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      I am Takasi Nyande, this is my first React application.
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
